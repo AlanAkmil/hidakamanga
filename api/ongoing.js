34d58135f80d7ca12ad7974fed72e02e5a32fa50
AlanAkmil <alansubagyo058@gmail.com>
2026-06-23 17:39:07 +0700

    Create ongoing.js

---
+++ api/ongoing.js
@@ -0,0 +1,11 @@
+const { scrapeGrid, BASE } = require('./_scraper');
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    const page = req.query.page || 1;
+    const data = await scrapeGrid(`${BASE}/ongoing-anime/page/${page}/`);
+    res.json({ ok: true, page: Number(page), data });
+  } catch (e) {
+    res.status(500).json({ ok: false, error: e.message });
+  }
+};
