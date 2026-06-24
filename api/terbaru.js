33a226a5eae09715b9d51e669d61b627ad4047ba
AlanAkmil <alansubagyo058@gmail.com>
2026-06-25 02:00:41 +0700

    Update terbaru.js

--- api/terbaru.js
+++ api/terbaru.js
@@ -1,11 +1,6 @@
-const { scrapeList, BASE } = require('./_scraper');
+const scraper = require('./_scraper');
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    const page = req.query.page || 1;
-    const data = await scrapeList(`${BASE}/terbaru/page/${page}/`);
-    res.json({ ok: true, page: Number(page), data });
-  } catch (e) {
-    res.status(500).json({ ok: false, error: e.message });
-  }
+  try { res.json(await scraper.home()); }
+  catch(e) { res.status(500).json({ ok:false, error:e.message }); }
 };
