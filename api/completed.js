43a292e15e34080fda17277df4ef35be56d4f904
AlanAkmil <alansubagyo058@gmail.com>
2026-06-29 01:51:38 +0700

    Update completed.js

--- api/completed.js
+++ api/completed.js
@@ -1,6 +1,6 @@
 const scraper = require('./_scraper');
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  try { res.json(await scraper.complete(parseInt(req.query.page)||1)); }
-  catch(e) { res.status(500).json({ ok:false, error:e.message }); }
+  try { res.json(await scraper.completed(parseInt(req.query.page)||1)); }
+  catch(e) { res.status(500).json({ok:false,error:e.message}); }
 };
