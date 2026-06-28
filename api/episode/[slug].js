a45ed3775d597430a66bb9c1d4724e5ea4ca02fb
AlanAkmil <alansubagyo058@gmail.com>
2026-06-29 01:49:44 +0700

    Delete [slug].js

--- api/episode/[slug].js
+++
@@ -1,9 +0,0 @@
-const scraper = require('../_scraper');
-module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    const slug = req.query.slug || '';
-    if (!slug) return res.status(400).json({ ok:false, error:'Missing slug' });
-    res.json(await scraper.episode(slug));
-  } catch(e) { res.status(500).json({ ok:false, error:e.message }); }
-};
