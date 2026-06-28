26c8d90e3940e52e8a2bff2f9ead31988cec560d
AlanAkmil <alansubagyo058@gmail.com>
2026-06-29 01:49:36 +0700

    Delete [genre].js

--- api/genre/[genre].js
+++
@@ -1,9 +0,0 @@
-const scraper = require('../_scraper');
-module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    const genre = req.query.genre || '';
-    if (!genre) return res.status(400).json({ ok:false, error:'Missing genre' });
-    res.json(await scraper.genre(genre, parseInt(req.query.page)||1));
-  } catch(e) { res.status(500).json({ ok:false, error:e.message }); }
-};
